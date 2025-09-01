d94b456df7b5c780c97f2462f394e368e2a5569d
rose86105218 <rose86105218@gmail.com>
2025-09-01 15:44:29 +0800

    Update get_danmu_tosql.py

--- data_ingestion/get_danmu_tosql.py
+++ data_ingestion/get_danmu_tosql.py
@@ -1,9 +1,10 @@
-# task_get_danmu
 import requests as req
 import json
 import pandas as pd
 import re
-from data_ingestion.worker import app
+from datetime import datetime, timezone, timedelta
+import time
+
 
 # 匯入video_list
 url = "https://raw.githubusercontent.com/rose86105218/AOT-research/main/video_sn.json"
@@ -23,36 +24,34 @@ def get_danmu(video):
     content = resp.text
     danmu = json.loads(content)
 
-    #此時data為一個list，[{彈幕1}, {彈幕2}, {彈幕3}]
-    data = danmu["data"]["danmu"]
+    # 此時df包含各則彈幕資訊，包含"text", "userid", "time", "sn"等
+    df = pd.DataFrame(danmu["data"]["danmu"])
 
-    # 初步資料處理
-    # 去除換行、加入集數、加入季、刪除多餘資訊
-    cleaned_data = []
-    for d in data:
-        d["text"] = d["text"].replace("\n","")
-        # 刪除標點等
-        d["text"] = re.sub(r'[^\w\u4e00-\u9fff/\\]', '', d["text"])
-        # 若清理後的彈幕是空白，就跳過不再處理
-        if d["text"] == "":
-            continue
-        cleaned_data.append(d)
+    # 清理文字
+    df["text"] = (
+        df["text"]
+        .str.replace("\n", "", regex=False)
+        .str.replace(r"[^\w\u4e00-\u9fff/\\]", "", regex=True)
+    )
 
+    # 去掉空白彈幕
+    df = df[df["text"] != ""]
 
-    df = pd.DataFrame(cleaned_data)
     # 增減欄位
-    df['uploaded_at'] = datetime.now(timezone.utc)  # 新增 uploaded_at 欄位，設為現在時間
+    # 資料取得時間 (+8時區)
+    utc_now = datetime.now(timezone.utc)
+    df['uploaded_at'] = utc_now.astimezone(timezone(timedelta(hours=8))).strftime("%Y-%m-%d %H:%M:%S")
+    df['uploaded_at'] = pd.to_datetime(df['uploaded_at'])
+
     df["season"] = video["season"]
     df["episode"] = video["episode"]
     df["season_episode"] = video["season_episode"]
     df["title"] = video["title"]
 
-    df = df.drop(column=["color, position, size"])
-    
+    df = df.drop(["color", "position", "size"], axis=1)
+
     upload_data_to_mysql(table_name="danmu", df=df)
     print("danmu has been uploaded to mysql.")
-
-
 
 if __name__ == "__main__":
     get_danmu(video_list[0])
